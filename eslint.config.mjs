f8272c0d7deff07fa42a771cc442f1ce5a6045d9
Siddant Rachha <siddant.rachha@gmail.com>
2025-02-20 16:38:27 +0530

    Remove ESLint configuration file; update dependencies and add TypeScript ESLint support

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,53 @@
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
+import prettier from 'eslint-plugin-prettier';
+import globals from 'globals';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
 import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
+const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
-];
+export default [
+  ...compat.extends(
+    'eslint:recommended',
+    'plugin:@next/next/recommended',
+    'plugin:prettier/recommended'
+  ),
+  {
+    plugins: {
+      prettier,
+    },
 
-export default eslintConfig;
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+
+      ecmaVersion: 12,
+      sourceType: 'module',
+
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+
+    rules: {
+      'prettier/prettier': [
+        'error',
+        {
+          endOfLine: 'auto',
+        },
+      ],
+
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
+];
