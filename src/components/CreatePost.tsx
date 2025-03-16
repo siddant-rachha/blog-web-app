7512116361f3327b5b906f1db0e86d5188ba5841
Siddant Rachha <siddant.rachha@gmail.com>
2025-03-16 16:43:00 +0530

    reset form functionality

--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -4,12 +4,35 @@ import { useAddPost } from '@/hooks/useAddPost';
 import { useAuthSlice } from '@/hooks/useAuthSlice';
 import { Box } from '@mui/material';
 import { BlogForm } from '@siddant-rachha/blog-components';
+import { useEffect, useState } from 'react';
 
 export const CreatePost = () => {
   const {
     selectors: { userDetails },
   } = useAuthSlice();
   const { handleFormAddPost } = useAddPost();
+
+  const [resetForm, setResetForm] = useState(false);
+
+  const handleFormSubmit = async (formData: {
+    imageFile: File | null;
+    title: string;
+    desc: string;
+    name: string;
+  }) => {
+    try {
+      await handleFormAddPost(formData);
+      setResetForm(true);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    if (resetForm) {
+      setResetForm(true);
+    }
+  }, [resetForm]);
 
   return (
     <Box
@@ -19,8 +42,9 @@ export const CreatePost = () => {
       }}
     >
       <BlogForm
-        handleFormSubmit={handleFormAddPost}
+        handleFormSubmit={handleFormSubmit}
         name={userDetails?.displayName || 'Anonymous'}
+        resetForm={resetForm}
       />
     </Box>
   );
