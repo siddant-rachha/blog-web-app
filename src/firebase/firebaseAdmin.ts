bb9dcba610e8a22ecb05a0f38112b3933453b469
Siddant Rachha <siddant.rachha@gmail.com>
2025-04-14 21:52:22 +0530

    fix-userdetails-img-not-updating

--- src/firebase/firebaseAdmin.ts
+++ src/firebase/firebaseAdmin.ts
@@ -1,21 +1,5 @@
 import admin from 'firebase-admin';
 
-// get FIREBASE_ADMIN_KEY object from firebase account
-// format
-// {
-//   "type": "",
-//   "project_id": "",
-//   "private_key_id": "",
-//   "private_key": "",
-//   "client_email": "",
-//   "client_id": "",
-//   "auth_uri": "",
-//   "token_uri": "",
-//   "auth_provider_x509_cert_url": "",
-//   "client_x509_cert_url": "",
-//   "universe_domain": ""
-// }
-
 const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_KEY || '{}');
 
 if (!admin.apps.length) {
