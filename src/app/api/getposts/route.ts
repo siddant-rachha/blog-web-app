a0636c7452d80996ce5e8694c8ae866ca6859df0
sid <68276495+siddant-rachha@users.noreply.github.com>
2025-09-22 03:31:37 +0530

    Backend pagination (#108)
    * added limit and cursors for backend pagination
    
    * type fix

--- src/app/api/getposts/route.ts
+++ src/app/api/getposts/route.ts
@@ -11,9 +11,16 @@ import { ADMIN_UID } from '../constants';
 // Fetch all posts or a single post by ID if 'id' is provided in the query string
 export async function GET(req: NextRequest) {
   try {
+    const { searchParams } = new URL(req.url);
+    const id = searchParams.get('id'); // Get id from query params
+    const myposts = searchParams.get('myposts'); // Get myposts from query params
+    const latest = searchParams.get('latest'); // Get desc from query params
+
+    const cursorId = searchParams.get('cursorId');
+    const limit = Number(searchParams.get('limit')) || 3;
+
     let uid = '';
     const authorizationHeader = req.headers.get('authorization')?.split(' ')[1];
-
     if (authorizationHeader) {
       try {
         const decodedToken = await decodeToken(authorizationHeader);
@@ -23,14 +30,9 @@ export async function GET(req: NextRequest) {
         return UNAUTHORIZED();
       }
     }
-
-    const { searchParams } = new URL(req.url);
-    const id = searchParams.get('id'); // Get id from query params
-    const myposts = searchParams.get('myposts'); // Get myposts from query params
-    const latest = searchParams.get('latest'); // Get desc from query params
-    let desc = true;
+    let descending = true;
     if (latest === 'false') {
-      desc = false;
+      descending = false;
     }
 
     if (id) {
@@ -66,13 +68,62 @@ export async function GET(req: NextRequest) {
           { status: 200 }
         );
       }
-      // Fetch only the posts created by the user
+      // ------ pagination ------
+      if (cursorId) {
+        const cursorDoc = await firebaseAdminDb
+          .collection('posts')
+          .doc(cursorId)
+          .get();
+        if (!cursorDoc.exists) {
+          return POST_NOT_FOUND();
+        }
+        // Fetch only the posts created by the user
+        // Firebase will throw error at first time and ask to create index
+        const postsSnapshot = await firebaseAdminDb
+          .collection('posts')
+          .where('uid', '==', uid) // For pagination
+          .orderBy('createdAt', descending ? 'desc' : 'asc') // Sort by latest
+          .startAfter(cursorDoc) // Start after the cursor document
+          .limit(limit) // Limit the number of results
+          .get();
 
+        if (postsSnapshot.empty) {
+          return NextResponse.json(
+            { posts: [], message: 'POST_NOT_FOUND' },
+            { status: 200 }
+          );
+        }
+
+        const posts = postsSnapshot.docs.map((doc) => {
+          const postData = doc.data();
+          return {
+            id: doc.id,
+            title: postData.title,
+            desc: postData.desc?.slice(0, 150),
+            author: postData?.author,
+            imageUrl: postData?.imageUrl,
+            authorPic: postData?.authorPic,
+            createdAt: postData?.createdAt,
+            writePermission: true,
+          };
+        });
+
+        return NextResponse.json(
+          { posts, message: 'POSTS_FETCHED' },
+          { status: 200 }
+        );
+      }
+      // ------ pagination ------
+
+      // --------------------
+
+      // Fetch only the posts created by the user
       // Firebase will throw error at first time and ask to create index
       const postsSnapshot = await firebaseAdminDb
         .collection('posts')
-        .where('uid', '==', uid)
-        .orderBy('createdAt', desc ? 'desc' : 'asc') // Sort by latest
+        .where('uid', '==', uid) // For pagination
+        .orderBy('createdAt', descending ? 'desc' : 'asc') // Sort by latest
+        .limit(limit) // Limit the number of results
         .get();
 
       if (postsSnapshot.empty) {
@@ -101,12 +152,62 @@ export async function GET(req: NextRequest) {
         { status: 200 }
       );
     } else {
+      // -------------- pagination ------
+      if (cursorId) {
+        const cursorDoc = await firebaseAdminDb
+          .collection('posts')
+          .doc(cursorId)
+          .get();
+        if (!cursorDoc.exists) {
+          return POST_NOT_FOUND();
+        }
+        const postsSnapshot = await firebaseAdminDb
+          .collection('posts')
+          .orderBy('createdAt', descending ? 'desc' : 'asc') // Sort by latest
+          .startAfter(cursorDoc) // Start after the cursor document
+          .limit(limit) // Limit the number of results
+          .get();
+
+        if (postsSnapshot.empty) {
+          return NextResponse.json(
+            { posts: [], message: 'POST_NOT_FOUND' },
+            { status: 200 }
+          );
+        }
+
+        const posts = postsSnapshot.docs.map((doc) => {
+          const postData = doc.data();
+          let writePermission = true;
+          if (postData.uid) {
+            writePermission = uid === postData.uid || uid === ADMIN_UID;
+          }
+          return {
+            id: doc.id,
+            title: postData.title,
+            desc: postData.desc?.slice(0, 150),
+            author: postData?.author,
+            imageUrl: postData?.imageUrl,
+            authorPic: postData?.authorPic,
+            createdAt: postData?.createdAt,
+            writePermission: writePermission,
+          };
+        });
+
+        return NextResponse.json(
+          { posts, message: 'POSTS_FETCHED' },
+          { status: 200 }
+        );
+      }
+
+      // ------- pagination -------
+
       // Fetch all posts if no ID is provided
 
       // Firebase will throw error at first time and ask to create index
       const postsSnapshot = await firebaseAdminDb
         .collection('posts')
-        .orderBy('createdAt', desc ? 'desc' : 'asc') // Sort by latest
+        .orderBy('createdAt', descending ? 'desc' : 'asc') // Sort by latest
+        .limit(limit) // Limit the number of results
         .get();
 
       if (postsSnapshot.empty) {
